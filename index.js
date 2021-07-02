d966303e7a7cb019adf04191c7c236be85871c8d
satty26 <65654262+satty26@users.noreply.github.com>
2021-07-02 12:36:08 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ $(document).ready(function () {
   }, 3000);
 });
 function updateTimer() {
-  future = Date.parse("june 08, 2021 00:00:00");
+  future = Date.parse("june 27, 2021 13:00:00");
   now = new Date();
   diff = future - now;
 
